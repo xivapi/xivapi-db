b98fd1b367693116daeee847017873bfffe0277b
Josh Freeman <josh@viion.co.uk>
2018-09-27 00:03:25 +0100

    We have nice search and pages!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,153 @@
 const $search = $('.search input');
+const $results = $('.results');
+const $main = $('main');
 
-let timeout = null;
-$search.on('keyup', function (event) {
+let timeout = null,
+    resetSearch = true,
+    searchString = '',
+    searchPage = 1,
+    searchIndexes = [
+        'item', 'recipe', 'achievement', 'action', 'quest',
+        'bnpcname', 'enpcresident', 'companion',
+        'mount', 'fate', 'leve', 'instancecontent', 'emote'
+    ],
+    canLoadMoreResults = false;
+
+// on searching
+$search.on('keyup', event => {
     let string = $(event.target).val().trim();
+
+    if (searchString !== string) {
+        searchString = string;
+        resetSearch  = true;
+        searchPage   = 1;
+    }
 
     clearTimeout(timeout);
     timeout = setTimeout(function() {
         $.ajax({
             url: 'https://xivapi.com/search',
             data: {
-                string: string,
-                limit: 8
+                string:     string,
+                limit:      100,
+                page:       searchPage,
+                indexes:    searchIndexes.join(','),
+                key:        'c859af13b62e465baa340a91'
             },
-            success: function (response) {
+            success: response => {
                 const results = response.Results;
-                const $auto = $('.auto');
+                canLoadMoreResults = (response.Pagination.PageNext !== null);
 
-                $auto.html('');
+                // reset if page 1
+                if (response.Pagination.Page === 1 || resetSearch) {
+                    $results.html('');
+                }
+
+                $results.append(`<div class="results-page">PAGE ${response.Pagination.Page}</div>`);
+
                 for (let i in results) {
                     let res = results[i];
-                    $auto.append('<div>' + res.Name + ' <small>'+ res._ +'</small></div>');
+
+                    $results.append(`
+                        <button id="LoadPage" class="res-${res.GameType}" data-id="${res._},${res.ID}">
+                            <div>
+                                <img src="https://xivapi.com/${res.Icon}">
+                                <img src="https://xivapi.com/${res.Icon}">
+                            </div>
+                            <div>
+                                <h3>${res.Name}</h3>
+                                <small>${res._}</small>
+                            </div>
+                             
+                        </button>
+                    `);
+                }
+
+                // if we can load more results, show button, incase browser detection is poop
+                if (canLoadMoreResults) {
+                    $('.canLoadMoreResults').addClass('on');
                 }
             },
-            error: function (a,b,c) {
+            error: (a,b,c) => {
                 console.error(a,b,c);
             }
         })
     }, 150);
+});
+
+// on clicking a thing
+$('html').on('click', '.results button', event => {
+    let info = $(event.currentTarget).attr('data-id').split(',');
+    $results.html('');
+
+    switch (info[0]) {
+        default:
+            load404(info[0]);
+            break;
+            
+        case 'item':
+            query(`https://xivapi.com/Item/${info[1]}`, loadItem);
+            break;
+    }
+});
+
+function query(url, callback) {
+    $.ajax({
+        url: url,
+        success: callback,
+        error: (a,b,c) => {
+            console.error(a,b,c);
+        }
+    });
+}
+
+// Page stuff
+function load404(type) {
+    $main.html(`${type} - idk what this is!`);
+}
+
+function loadItem(item) {
+    $main.html('');
+
+    // top
+    $main.append(`
+        <div class="page-top">
+            <span>Patch ${item.GamePatch.Version}</span>
+            <div class="page-top-frame">
+                <img src="http://xivapi.com/${item.Icon}">
+                <div>
+                    <h2>${item.Name}</h2>
+                    <div>
+                        ${item.ItemKind.Name} - ${item.ItemUICategory.Name}
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    `);
+}
+
+setTimeout(() => {
+    query('https://xivapi.com/Item/1675', loadItem);
+}, 500);
+
+// Auto load results on scroll
+$(window).on('scroll', event => {
+    if (!canLoadMoreResults) {
+        return;
+    }
+
+    const $button           = $('.more > button');
+    const top_of_element    = $button.offset().top;
+    const bottom_of_element = $button.offset().top + $button.outerHeight();
+    const bottom_of_screen  = $(window).scrollTop() + window.innerHeight;
+    const top_of_screen     = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+        searchPage++;
+        canLoadMoreResults = false;
+        resetSearch = false;
+        $('.canLoadMoreResults').removeClass('on');
+        $search.trigger('keyup');
+    }
 });
--- public/ui/app.js
+++ public/ui/app.js
@@ -75,10 +75,25 @@ var xiv =
 /***/ (function(module, exports) {
 
 var $search = $('.search input');
+var $results = $('.results');
+var $main = $('main');
 
-var timeout = null;
+var timeout = null,
+    resetSearch = true,
+    searchString = '',
+    searchPage = 1,
+    searchIndexes = ['item', 'recipe', 'achievement', 'action', 'quest', 'bnpcname', 'enpcresident', 'companion', 'mount', 'fate', 'leve', 'instancecontent', 'emote'],
+    canLoadMoreResults = false;
+
+// on searching
 $search.on('keyup', function (event) {
     var string = $(event.target).val().trim();
+
+    if (searchString !== string) {
+        searchString = string;
+        resetSearch = true;
+        searchPage = 1;
+    }
 
     clearTimeout(timeout);
     timeout = setTimeout(function () {
@@ -86,16 +101,31 @@ $search.on('keyup', function (event) {
             url: 'https://xivapi.com/search',
             data: {
                 string: string,
-                limit: 8
+                limit: 100,
+                page: searchPage,
+                indexes: searchIndexes.join(','),
+                key: 'c859af13b62e465baa340a91'
             },
             success: function success(response) {
                 var results = response.Results;
-                var $auto = $('.auto');
+                canLoadMoreResults = response.Pagination.PageNext !== null;
 
-                $auto.html('');
+                // reset if page 1
+                if (response.Pagination.Page === 1 || resetSearch) {
+                    $results.html('');
+                }
+
+                $results.append('<div class="results-page">PAGE ' + response.Pagination.Page + '</div>');
+
                 for (var i in results) {
                     var res = results[i];
-                    $auto.append('<div>' + res.Name + ' <small>' + res._ + '</small></div>');
+
+                    $results.append('\n                        <button id="LoadPage" class="res-' + res.GameType + '" data-id="' + res._ + ',' + res.ID + '">\n                            <div>\n                                <img src="https://xivapi.com/' + res.Icon + '">\n                                <img src="https://xivapi.com/' + res.Icon + '">\n                            </div>\n                            <div>\n                                <h3>' + res.Name + '</h3>\n                                <small>' + res._ + '</small>\n                            </div>\n                             \n                        </button>\n                    ');
+                }
+
+                // if we can load more results, show button, incase browser detection is poop
+                if (canLoadMoreResults) {
+                    $('.canLoadMoreResults').addClass('on');
                 }
             },
             error: function error(a, b, c) {
@@ -103,6 +133,69 @@ $search.on('keyup', function (event) {
             }
         });
     }, 150);
+});
+
+// on clicking a thing
+$('html').on('click', '.results button', function (event) {
+    var info = $(event.currentTarget).attr('data-id').split(',');
+    $results.html('');
+
+    switch (info[0]) {
+        default:
+            load404(info[0]);
+            break;
+
+        case 'item':
+            query('https://xivapi.com/Item/' + info[1], loadItem);
+            break;
+    }
+});
+
+function query(url, callback) {
+    $.ajax({
+        url: url,
+        success: callback,
+        error: function error(a, b, c) {
+            console.error(a, b, c);
+        }
+    });
+}
+
+// Page stuff
+function load404(type) {
+    $main.html(type + ' - idk what this is!');
+}
+
+function loadItem(item) {
+    $main.html('');
+
+    // top
+    $main.append('\n        <div class="page-top">\n            <span>Patch ' + item.GamePatch.Version + '</span>\n            <div class="page-top-frame">\n                <img src="http://xivapi.com/' + item.Icon + '">\n                <div>\n                    <h2>' + item.Name + '</h2>\n                    <div>\n                        ' + item.ItemKind.Name + ' - ' + item.ItemUICategory.Name + '\n                    </div>\n                </div>\n            </div>\n            \n        </div>\n    ');
+}
+
+setTimeout(function () {
+    query('https://xivapi.com/Item/1675', loadItem);
+}, 500);
+
+// Auto load results on scroll
+$(window).on('scroll', function (event) {
+    if (!canLoadMoreResults) {
+        return;
+    }
+
+    var $button = $('.more > button');
+    var top_of_element = $button.offset().top;
+    var bottom_of_element = $button.offset().top + $button.outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+    var top_of_screen = $(window).scrollTop();
+
+    if (bottom_of_screen > top_of_element && top_of_screen < bottom_of_element) {
+        searchPage++;
+        canLoadMoreResults = false;
+        resetSearch = false;
+        $('.canLoadMoreResults').removeClass('on');
+        $search.trigger('keyup');
+    }
 });
 
 /***/ })
